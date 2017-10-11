6f435932904d3abf20ae383f8547197e187617b7
netsfx <pivxinfo@gmail.com>
2017-10-11 20:42:25 +0200

    Use .Any

--- Blockexplorer/Program.cs
+++ Blockexplorer/Program.cs
@@ -13,8 +13,8 @@ namespace Blockexplorer
 			var host = new WebHostBuilder()
 				.UseKestrel(options =>
 				{
-					options.Listen(IPAddress.Loopback, 80);
-					options.Listen(IPAddress.Loopback, 443,
+					options.Listen(IPAddress.Any, 80);
+					options.Listen(IPAddress.Any, 443,
 				   listenOptions =>
 						{
 							try
