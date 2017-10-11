c85ef7126a850ed3149f6de27898e22e779f70d5
netsfx <pivxinfo@gmail.com>
2017-10-11 19:37:32 +0200

    Add https

--- Blockexplorer/Program.cs
+++ Blockexplorer/Program.cs
@@ -1,26 +1,40 @@
-﻿using System.IO;
+﻿using System;
+using System.IO;
+using System.Net;
 using System.Reflection;
 using Microsoft.AspNetCore.Hosting;
 
 namespace Blockexplorer
 {
-    public class Program
-    {
-        public static void Main(string[] args)
-        {
-            var host = new WebHostBuilder()
-                .UseKestrel()
-	            .UseUrls("http://*:80")
+	public class Program
+	{
+		public static void Main(string[] args)
+		{
+			var host = new WebHostBuilder()
+				.UseKestrel(options =>
+				{
+					options.Listen(IPAddress.Any, 443,
+				   listenOptions =>
+						{
+							try
+							{
+								listenOptions.UseHttps("wildcard_obsidianplatform_com.pfx", args[0]); // hk
+							}
+							catch (Exception) { }
+						});
+					options.Listen(IPAddress.Any,80);
+				})
+				.UseUrls("http://*:80")
 				.UseContentRoot(Directory.GetCurrentDirectory())
-                .UseIISIntegration()
-                .UseStartup<Startup>()
-                //.UseApplicationInsights()
-				
-                .Build();
+				.UseIISIntegration()
+				.UseStartup<Startup>()
+				//.UseApplicationInsights()
 
-            host.Run();
-        }
+				.Build();
 
-	    public static string Version => Assembly.GetEntryAssembly().GetCustomAttribute<AssemblyInformationalVersionAttribute>().InformationalVersion;
-    }
+			host.Run();
+		}
+
+		public static string Version => Assembly.GetEntryAssembly().GetCustomAttribute<AssemblyInformationalVersionAttribute>().InformationalVersion;
+	}
 }
