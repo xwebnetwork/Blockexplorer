6e948ff5343ac013664bec36a3240b0f9168ddde
jacob zlogar <jacob.zlogar@gmail.com>
2017-10-16 13:54:11 -0400

    Update

--- Blockexplorer/Controllers/ApiController.cs
+++ Blockexplorer/Controllers/ApiController.cs
@@ -6,58 +6,61 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace Blockexplorer.Controllers
 {
-  public class ApiController : Controller
-  {
-    readonly IApiService _apiService;
+    public class ApiController : Controller
+    {
+        readonly IApiService _apiService;
 
-    public ApiController(IApiService apiService)
-    {
-      _apiService = apiService;
+        public ApiController(IApiService apiService)
+        {
+            _apiService = apiService;
+        }
+
+        // [HttpGet]
+        public async Task<ActionResult> GetInfo()
+        {
+            try
+            {
+                var info = await _apiService.GetInfo();
+                if (info == null || !string.IsNullOrWhiteSpace(info.Errors))
+                    return StatusCode(StatusCodes.Status500InternalServerError);
+                return Json(new { info = info });
+            }
+            catch (Exception)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError);
+            }
+        }
+
+        // [HttpGet]
+        public async Task<ActionResult> MoneySupplyJson()
+        {
+            try
+            {
+                var info = await _apiService.GetInfo();
+                if (info == null || !string.IsNullOrWhiteSpace(info.Errors))
+                    return StatusCode(StatusCodes.Status500InternalServerError);
+                return Json(new { moneySupply = Convert.ToInt32(info.MoneySupply) });
+            }
+            catch (Exception)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError);
+            }
+        }
+
+        // [HttpGet]
+        public async Task<string> MoneySupplyString()
+        {
+            try
+            {
+                var info = await _apiService.GetInfo();
+                if (info == null || !string.IsNullOrWhiteSpace(info.Errors))
+                    return "-1";
+                return Convert.ToInt32(info.MoneySupply).ToString();
+            }
+            catch (Exception)
+            {
+                return "-1";
+            }
+        }
     }
-    [HttpGet]
-    public async Task<ActionResult> GetInfo()
-    {
-      try
-      {
-        var info = await _apiService.GetInfo();
-        if (info == null || !string.IsNullOrWhiteSpace(info.Errors))
-          return StatusCode(StatusCodes.Status500InternalServerError);
-        return Json(new { info = info });
-      }
-      catch (Exception)
-      {
-        return StatusCode(StatusCodes.Status500InternalServerError);
-      }
-    }
-    [HttpGet]
-    public async Task<ActionResult> MoneySupplyJson()
-    {
-      try
-      {
-        var info = await _apiService.GetInfo();
-        if (info == null || !string.IsNullOrWhiteSpace(info.Errors))
-          return StatusCode(StatusCodes.Status500InternalServerError);
-        return Json(new { moneySupply = Convert.ToInt32(info.MoneySupply) });
-      }
-      catch (Exception)
-      {
-        return StatusCode(StatusCodes.Status500InternalServerError);
-      }
-    }
-    [HttpGet]
-    public async Task<string> MoneySupplyString()
-    {
-      try
-      {
-        var info = await _apiService.GetInfo();
-        if (info == null || !string.IsNullOrWhiteSpace(info.Errors))
-          return "-1";
-        return Convert.ToInt32(info.MoneySupply).ToString();
-      }
-      catch (Exception)
-      {
-        return "-1";
-      }
-    }
-  }
 }
