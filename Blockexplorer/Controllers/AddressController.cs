aa1b4d7388aa1fd7747dc8f661e2103f9acb92fa
netsfx <pivxinfo@gmail.com>
2018-08-10 18:54:08 +0200

    Cache toplist

--- Blockexplorer/Controllers/AddressController.cs
+++ Blockexplorer/Controllers/AddressController.cs
@@ -6,92 +6,93 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace Blockexplorer.Controllers
 {
-    public class AddressController : Controller
-    {
-        readonly IAddressService _addressService;
-        readonly IBlockService _blockService;
+	public class AddressController : Controller
+	{
+		readonly IAddressService _addressService;
+		readonly IBlockService _blockService;
 
-        const int ItemsOnPage = 20;
+		const int ItemsOnPage = 20;
 
-        public AddressController(IAddressService addressService, IBlockService blockService)
-        {
-            _addressService = addressService;
-            _blockService = blockService;
-        }
+		public AddressController(IAddressService addressService, IBlockService blockService)
+		{
+			_addressService = addressService;
+			_blockService = blockService;
+		}
 
-        [Route("address/{id}")]
-        public async Task<ActionResult> Index(string id, string show, int page = 0)
-        {
-            //if(show == null)
-            //	return View("_NotFound");
+		[Route("address/{id}")]
+		public async Task<ActionResult> Index(string id, string show, int page = 0)
+		{
+			//if(show == null)
+			//	return View("_NotFound");
 
-            try
-            {
-                if (string.IsNullOrEmpty(id))
-                {
-                    return RedirectToAction("Index", "Home");
-                }
+			try
+			{
+				if (string.IsNullOrEmpty(id))
+				{
+					return RedirectToAction("Index", "Home");
+				}
 
-                var address = await _addressService.GetAddress(id);
+				var address = await _addressService.GetAddress(id);
 
-                if (address == null)
-                {
-                    return View("_NotFound");
-                }
+				if (address == null)
+				{
+					return View("_NotFound");
+				}
 
-                var start = ItemsOnPage * page;
+				var start = ItemsOnPage * page;
 
-                long max;
-                if (start < address.TotalTransactions && start + ItemsOnPage < address.TotalTransactions)
-                {
-                    max = start + ItemsOnPage;
-                }
-                else
-                {
-                    max = address.TotalTransactions;
-                }
+				long max;
+				if (start < address.TotalTransactions && start + ItemsOnPage < address.TotalTransactions)
+				{
+					max = start + ItemsOnPage;
+				}
+				else
+				{
+					max = address.TotalTransactions;
+				}
 
-                var vm = new AddressModel
-                {
-                    Address = address,
-                    Count = (int)Math.Ceiling((decimal)address.TotalTransactions / ItemsOnPage),
-                    CurrentPage = page,
-                    Start = start,
-                    Max = max
-                };
+				var vm = new AddressModel
+				{
+					Address = address,
+					Count = (int)Math.Ceiling((decimal)address.TotalTransactions / ItemsOnPage),
+					CurrentPage = page,
+					Start = start,
+					Max = max
+				};
 
-                return View(vm);
-            }
-            catch (Exception e)
-            {
-                return View("_NotFound");
-            }
+				return View(vm);
+			}
+			catch (Exception e)
+			{
+				return View("_NotFound");
+			}
 
-        }
+		}
 
-        [Route("address/top")]
-        public async Task<ActionResult> Top()
-        {
-            try
-            {
-                var top = await _addressService.GetTopList();
-                var stats = await _addressService.GetStats();
-                var lastBlock = await _blockService.GetLastBlock();
+		[Route("address/top")]
+		[ResponseCache(Duration = 120, VaryByQueryKeys = new[] { "clear" })]
+		public async Task<ActionResult> Top()
+		{
+			try
+			{
+				var top = await _addressService.GetTopList();
+				var stats = await _addressService.GetStats();
+				var lastBlock = await _blockService.GetLastBlock();
 
-                int indexerHeight = stats.Item1;
-                if (indexerHeight > lastBlock.Height) // best blockheight is cached and may thus lag indexer height
-                    indexerHeight = (int)lastBlock.Height;
+				int indexerHeight = stats.Item1;
+				if (indexerHeight > lastBlock.Height) // best blockheight is cached and may thus lag indexer height
+					indexerHeight = (int)lastBlock.Height;
 
-                ViewData["BestHeight"] = indexerHeight;
-                ViewData["IndexerLastSeenAgo"] = (DateTime.UtcNow - stats.Item2).TotalMinutes.ToString("0.0");
-                ViewData["BestBlockHeight"] = lastBlock.Height;
-                return View(top);
-            }
-            catch (Exception e)
-            {
-                return View("Error");
-            }
+				ViewData["BestHeight"] = indexerHeight;
+				ViewData["IndexerLastSeenAgo"] = (DateTime.UtcNow - stats.Item2).TotalMinutes.ToString("0.0");
+				ViewData["BestBlockHeight"] = lastBlock.Height;
+				return View(top);
+			}
+			catch (Exception e)
+			{
+				return View("Error");
+			}
 
-        }
-    }
+		}
+	}
 }
