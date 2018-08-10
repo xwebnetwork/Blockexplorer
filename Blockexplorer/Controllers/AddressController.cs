9def3879051df9b5903cca06aa1ceb2d2d53cd65
netsfx <pivxinfo@gmail.com>
2018-08-10 19:22:15 +0200

    Manual data caching

--- Blockexplorer/Controllers/AddressController.cs
+++ Blockexplorer/Controllers/AddressController.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Threading.Tasks;
 using Blockexplorer.Core.Interfaces.Services;
 using Blockexplorer.Models;
@@ -69,30 +70,52 @@ namespace Blockexplorer.Controllers
 
 		}
 
+		static TopListData TopListCache;
+
 		[Route("address/top")]
 		[ResponseCache(Duration = 120)]
 		public async Task<ActionResult> Top()
 		{
 			try
 			{
-				var top = await _addressService.GetTopList();
-				var stats = await _addressService.GetStats();
-				var lastBlock = await _blockService.GetLastBlock();
+				TopListData data;
+				if (TopListCache != null && DateTime.UtcNow - TopListCache.DateTime <= TimeSpan.FromSeconds(120))
+					data = TopListCache;
+				else
+				{
+					data = new TopListData()
+					{
+						DateTime = DateTime.UtcNow,
+						TopList = await _addressService.GetTopList(),
+						Stats = await _addressService.GetStats(),
+						LastBlock = await _blockService.GetLastBlock()
+					};
+					TopListCache = data;
+				}
 
-				int indexerHeight = stats.Item1;
-				if (indexerHeight > lastBlock.Height) // best blockheight is cached and may thus lag indexer height
-					indexerHeight = (int)lastBlock.Height;
+				int indexerHeight = data.Stats.Item1;
+				if (indexerHeight > data.LastBlock.Height) // best blockheight is cached and may thus lag indexer height
+					indexerHeight = (int)data.LastBlock.Height;
 
 				ViewData["BestHeight"] = indexerHeight;
-				ViewData["IndexerLastSeenAgo"] = (DateTime.UtcNow - stats.Item2).TotalMinutes.ToString("0.0");
-				ViewData["BestBlockHeight"] = lastBlock.Height;
-				return View(top);
+				ViewData["IndexerLastSeenAgo"] = (DateTime.UtcNow - data.Stats.Item2).TotalMinutes.ToString("0.0");
+				ViewData["BestBlockHeight"] = data.LastBlock.Height;
+				return View(data.TopList);
 			}
 			catch (Exception e)
 			{
 				return View("Error");
 			}
 
+		}
+
+		class TopListData
+		{
+			public DateTime DateTime;
+
+			public List<Core.Domain.Address> TopList;
+			public Tuple<int, DateTime> Stats;
+			public Core.Domain.Block LastBlock;
 		}
 	}
 }
